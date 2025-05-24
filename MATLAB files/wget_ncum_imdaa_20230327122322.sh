22fa1ea282e57c2b23be5d297abfa93f9c0c27a1
Agami Pramanik <agamip@iisc.ac.in>
2025-05-24 16:18:29 +0530

    Add files via upload

---
+++ MATLAB files/wget_ncum_imdaa_20230327122322.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+HOST=https://rds.ncmrwf.gov.in
+
+wget -c $HOST/clcFUpW6ZmbcziVOSpLSDw/NtgCOr7u_5rKQgZ3iv39t7s2lScQD7VKgAnAH8UrN3Ot40LVoGznzD0yidxgEZE1HJv38gjTFcBOcd68oR-7TQ.nc -O ncum_imdaa_reanl_HR_APCP-sfc_2000040100-2000103123.nc
+
+wget -c $HOST/clcFUpW6ZmbcziVOSpLSDw/3hXP1SZDKCjwIqDRh5dxroj5qdqJVw9VSv6_z4NpRoEkGgOJi7nCLvlwZO1rKSMOtQ8KnFgOXuRwVEGKTcG9xg.nc -O ncum_imdaa_reanl_HR_APCP-sfc_2001040100-2001103123.nc
+
+wget -c $HOST/clcFUpW6ZmbcziVOSpLSDw/SdqDjiwQMeOhdH14aiF-wfWt3avhdaQaagCm7d1JzJudcqQIADLHh17yTD-6GDxPA7FC4e0oGMdd6wEpq1nqOg.nc -O ncum_imdaa_reanl_HR_APCP-sfc_2002040100-2002103123.nc
+
+wget -c $HOST/clcFUpW6ZmbcziVOSpLSDw/DvO48iR-hmvvAz2uGi7UqLSTdjGfucStG5H92Jl-OngYv0iBaTUg6S4kKK5khQ-yhGTxy7RTMxGhmal-cKbSXw.nc -O ncum_imdaa_reanl_HR_APCP-sfc_2003040100-2003103123.nc
+
+wget -c $HOST/clcFUpW6ZmbcziVOSpLSDw/Ozu2s-AHKvs1MIOe9Ai3B0fLuoFAdS5Cj4ZRZ_4zNRilTl--VysXKcvLF6VW7X77-tdYlucvnWgA_5JvFSFcwA.nc -O ncum_imdaa_reanl_HR_APCP-sfc_2004040100-2004103123.nc
+
+wget -c $HOST/clcFUpW6ZmbcziVOSpLSDw/r5LMB9pjYbgdbibTL7JngAuup3UrQr6cejDP9fZUvGGv1mbKl2R3-NRr_ZeBwwHx6MLlqIRu0OmZiMiPwMLiww.nc -O ncum_imdaa_reanl_HR_APCP-sfc_2005040100-2005103123.nc
+
+wget -c $HOST/clcFUpW6ZmbcziVOSpLSDw/XrfYsiTt1FjmRl22VlQVQzhrvCeZU46GWd2AStTbd6GhZ-wA4jn2OuCZjbE0rxy5wlOpQT6VAp70PCa-MG29yQ.nc -O ncum_imdaa_reanl_HR_APCP-sfc_2006040100-2006103123.nc
+
+wget -c $HOST/clcFUpW6ZmbcziVOSpLSDw/D7uQ9q97oV0O6I-lVJ1jBMFPMJSeBVtnRA2BwaiBRaIeflKlzD2WJ3gX8aSkq3V1YNGEJz-duJJ18qilVTBM4Q.nc -O ncum_imdaa_reanl_HR_APCP-sfc_2007040100-2007103123.nc
+
+wget -c $HOST/clcFUpW6ZmbcziVOSpLSDw/Q5hrekXvi8c7QO5OnEF7MMyU4WaLCslieiKp4ZWbK_oeJ2KfeKmk8f9WoRdk6Noq666gccnF0RE-nbyUukhfog.nc -O ncum_imdaa_reanl_HR_APCP-sfc_2008040100-2008103123.nc
+
+wget -c $HOST/clcFUpW6ZmbcziVOSpLSDw/wL4xEhwFhHGn-HhHcB2Q4URqB_zg2hPWAuGCgkqzi56knqVgXc2REDyNbTLyczayiJSu5itZ4kMCTPlBuBcNJw.nc -O ncum_imdaa_reanl_HR_APCP-sfc_2009040100-2009103123.nc
+
+echo 'Downloaded all files. '
+echo 'Total downlaoded size is 1.4G.'
+echo 'Bye!'
+exit;
